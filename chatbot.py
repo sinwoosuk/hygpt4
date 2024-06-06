bbc180df07bc041ca962704eb847147fc40d3447
sinwoosuk <166152695+sinwoosuk@users.noreply.github.com>
2024-06-07 00:58:39 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,8 @@
 __import__("pysqlite3")
 import sys
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-
 # from dotenv import load_dotenv
 # load_dotenv()
-
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Chroma
@@ -13,29 +11,24 @@ import streamlit as st
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.document_loaders import DirectoryLoader
 
-from PIL import Image
 
 # CSVLoader 확장
 class CustomCSVLoader(CSVLoader):
     def __init__(self, file_path, encoding="CP949", **kwargs):
         super().__init__(file_path, encoding=encoding, **kwargs)
-
 # DirectoryLoader 사용
-loader = DirectoryLoader("/hy", glob="*.csv", loader_cls=CustomCSVLoader)
-
+loader = DirectoryLoader("hy", glob="*.csv", loader_cls=CustomCSVLoader)
 # # CSV파일 불러오기
 # loader = DirectoryLoader("/hy", glob="*.csv", loader_cls=CSVLoader)
-
-
+# CSV파일 불러오기
+# loader = DirectoryLoader("./hy", glob="*.csv", loader_cls=CSVLoader)
 # loader = CSVLoader(file_path="hy/professor.csv", encoding="CP949")
 data = loader.load()
-
 # OpenAI Embedding 모델을 이용해서 Chunk를 Embedding 한후 Vector Store에 저장
 vectorstore = Chroma.from_documents(
     documents=data, embedding=OpenAIEmbeddings()
 )
 retriever = vectorstore.as_retriever()
-
 # 템플릿 객체 생성
 template = """
 다음과 같은 맥락을 사용하여 마지막 질문에 대답하십시오.
@@ -45,28 +38,20 @@ template = """
 질문: {question}
 도움이 되는 답변:"""
 rag_prompt_custom = PromptTemplate.from_template(template)
-
 # GPT-3.5 trurbo를 이용해서 LLM 설정
 from langchain.chat_models import ChatOpenAI
-
-llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
-
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 # RAG chain 설정
 from langchain.schema.runnable import RunnablePassthrough
-
 rag_chain = (
     {"context": retriever, "question": RunnablePassthrough()} | rag_prompt_custom | llm
 )
 
 # print(rag_chain.invoke(''))
 
-img = Image.open('/logo.png')
-st.image(img)
-
-st.title("한영대GPT")
+st.title("한영대 GPT")
 content = st.text_input("한영대에 관련된 질문을 입력하세요!")
 if st.button("요청하기"):
     with st.spinner("답변 생성 중..."):
         result = rag_chain.invoke(content)
         st.write(result.content)
-
