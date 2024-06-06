187cd9a1ecc656c614accef6a86160891bcb8b53
sinwoosuk <166152695+sinwoosuk@users.noreply.github.com>
2024-06-07 01:09:28 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -11,19 +11,16 @@ import streamlit as st
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.document_loaders import DirectoryLoader
 from PIL import Image
-
+import os
 
 # CSVLoader 확장
 class CustomCSVLoader(CSVLoader):
     def __init__(self, file_path, encoding="CP949", **kwargs):
         super().__init__(file_path, encoding=encoding, **kwargs)
+
 # DirectoryLoader 사용
 loader = DirectoryLoader("hy", glob="*.csv", loader_cls=CustomCSVLoader)
-# # CSV파일 불러오기
-# loader = DirectoryLoader("/hy", glob="*.csv", loader_cls=CSVLoader)
 # CSV파일 불러오기
-# loader = DirectoryLoader("./hy", glob="*.csv", loader_cls=CSVLoader)
-# loader = CSVLoader(file_path="hy/professor.csv", encoding="CP949")
 data = loader.load()
 # OpenAI Embedding 모델을 이용해서 Chunk를 Embedding 한후 Vector Store에 저장
 vectorstore = Chroma.from_documents(
@@ -41,16 +38,16 @@ template = """
 rag_prompt_custom = PromptTemplate.from_template(template)
 # GPT-3.5 trurbo를 이용해서 LLM 설정
 from langchain.chat_models import ChatOpenAI
-llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+llm = ChatOpenAI(model_name="gpt-4", temperature=0)
 # RAG chain 설정
 from langchain.schema.runnable import RunnablePassthrough
 rag_chain = (
     {"context": retriever, "question": RunnablePassthrough()} | rag_prompt_custom | llm
 )
 
-# print(rag_chain.invoke(''))
-
-img = Image.open('/logo.png')
+# 이미지 파일 경로 수정
+img_path = os.path.join(os.getcwd(), 'logo.png')
+img = Image.open(img_path)
 st.image(img)
 st.title("한영대 GPT")
 content = st.text_input("한영대에 관련된 질문을 입력하세요!")
